4420c29ffc145758ab82d30671290e89b0622a9a
akashgh05 <124536216+akashgh05@users.noreply.github.com>
2024-03-06 01:21:26 +0530

    install.sh

--- install.sh
+++ install.sh
@@ -54,11 +54,6 @@ elif [[ "${release}" == "fedora" ]]; then
         echo -e "${red} Please use Fedora 36 or higher version!${plain}\n" && exit 1
     fi
 
-elif [[ "${release}" == "debian" ]]; then
-    if [[ ${os_version} -lt 11 ]]; then
-        echo -e "${red} Please use Debian 11 or higher ${plain}\n" && exit 1
-    fi
-
 elif [[ "${release}" == "almalinux" ]]; then
     if [[ ${os_version} -lt 9 ]]; then
         echo -e "${red} Please use AlmaLinux 9 or higher ${plain}\n" && exit 1
